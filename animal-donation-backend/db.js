9bbeabaacbd40cc0336b2e5d89faf54666f76235
Joao <148392619+bosconogueiramd@users.noreply.github.com>
2024-09-24 21:59:17 -0300

    Primeiro commit do projeto de doação de animais

--- animal-donation-backend/db.js
+++ animal-donation-backend/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+<<<<<<< HEAD
 const connection = mysql.createConnection({
     host: 'YOUR_DB_HOST',  // Exemplo: 'localhost' ou o endereço do servidor Back4App
     user: 'YOUR_DB_USER',  // Seu usuário do MySQL
@@ -7,6 +8,18 @@ const connection = mysql.createConnection({
     database: 'YOUR_DB_NAME'  // O nome do banco de dados que você criou
 });
 
+=======
+// Configura a conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: '127.0.0.1',      // Endereço do host
+    port: 3306,             // Porta do MySQL
+    user: 'bosconogueira',  // Nome de usuário do banco de dados
+    password: 'bosconogueira*1', // Senha do banco de dados
+    database: 'animaldonation'    // Nome do banco de dados (schema)
+});
+
+// Conecta ao banco de dados
+>>>>>>> 73e24ac (Adicionando o projeto de doação de animais)
 connection.connect(error => {
     if (error) {
         console.error('Erro ao conectar ao banco de dados:', error);
@@ -15,4 +28,8 @@ connection.connect(error => {
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
 });
 
+<<<<<<< HEAD
+=======
+// Exporta a conexão para ser utilizada em outros arquivos
+>>>>>>> 73e24ac (Adicionando o projeto de doação de animais)
 module.exports = connection;
